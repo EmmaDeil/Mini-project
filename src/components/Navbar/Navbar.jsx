93da917573809f7471e43283838e928a7af9f9f8
David Emmanuel <eclefzy@gmail.com>
2025-09-02 13:05:52 +0100

    I have been able to correct the navbar responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,101 +2,93 @@ import React from "react";
 import { Link } from "react-router-dom";
 import CartBtn from "../Button/cartBtn";
 
-
 const Navbar = () => {
   return (
-    <div>
-        {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div class="container-fluid px-2 navlist">
-            <Link to="/" class="navbar-brand">
-              S Store
-            </Link>
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNavDropdown"
-              aria-controls="navbarNavDropdown"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown navbarNav">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <Link to="/" class="nav-link active" aria-current="page">
-                    Home
-                  </Link>
+    <nav class="navbar navbar-expand-lg" id="navfluid">
+      <div class="container-fluid">
+        <Link to="/" class="navbar-brand">
+          <img
+            src="/logo.png"
+            alt="Logo"
+            width="30"
+            height="28"
+            class="d-inline-block align-text-top"
+          />
+          OneStop~Shop
+        </Link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mx-auto mb-2 mb-lg-0" id="navbarNav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <Link to="/product" class="nav-link">
+                Product
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link to="/about" class="nav-link">
+                About
+              </Link>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" aria-disabled="true">
+                Our Partners
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <i class="fa fa-question-circle" aria-hidden="true"></i> Help
+              </a>
+              <ul class="dropdown-menu">
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Action
+                  </a>
                 </li>
-                <li class="nav-item">
-                  <Link to="/product" class="nav-link">
-                    Product
-                  </Link>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Another action
+                  </a>
                 </li>
-                <li class="nav-item">
-                  <Link to="/about" class="nav-link">
-                    About
-                  </Link>
+                <li>
+                  <hr class="dropdown-divider" />
                 </li>
-                <li class="nav-item">
-                  <Link to="/contact" class="nav-link">
-                    Contact
-                  </Link>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Something else here
+                  </a>
                 </li>
-                <CartBtn/>
               </ul>
-            </div>
-          </div>
-        </nav> */}
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <Link to="/" class="navbar-brand">
-          <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top" />
-      OneStop~Shop
-    </Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0" id="navbarNav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-                  <Link to="/product" class="nav-link">
-                    Product
-                  </Link>
-                </li>
-                <li class="nav-item">
-                  <Link to="/about" class="nav-link">
-                    About
-                  </Link>
-                </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <i class="fa fa-question-circle" aria-hidden="true"></i> Help 
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </li>
           </ul>
-        </li>
-        <CartBtn/>
-      </ul>
-      <form class="d-flex" role="search">
+          <CartBtn />
+          {/* <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-    </div>
+      </form> */}
+        </div>
+      </div>
+    </nav>
   );
 };
 
